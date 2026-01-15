fdcc747218e3108f17fb577c5b697f0fee41ab0d
naiar <naiara.diaz101@alu.ulpgc.es>
2026-01-15 18:43:39 +0000

    implemented web search and automatic database loading

---
+++ src/main/java/software/ulpgc/kata6/application/DatabaseMovieStore.java
@@ -0,0 +1,66 @@
+package software.ulpgc.kata6.application;
+
+import software.ulpgc.kata6.architecture.io.Store;
+import software.ulpgc.kata6.architecture.model.Movie;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DatabaseMovieStore {
+    private final Connection connection;
+    private static final String database = "movies.db";
+    public static DatabaseMovieStore with(Connection connection) throws SQLException {
+        if (isDatabaseEmpty()) importMoviesInto(connection);
+        return new DatabaseMovieStore(connection);
+
+    }
+
+    private static void importMoviesInto(Connection connection) throws SQLException {
+        connection.setAutoCommit(false);
+        Stream<Movie> movies = new RemoteStore(MovieDeserializer::fromTsv).movies();
+        new DatabaseRecorder(connection).record(movies);
+        new DatabaseRecorder(connection).record(movies);
+
+        connection.createStatement().execute("CREATE INDEX IF NOT EXISTS idx_year ON movies(year)");
+        connection.commit();
+        connection.setAutoCommit(true);
+    }
+
+    private static boolean isDatabaseEmpty() {
+        return new File(database).length() == 0;
+    }
+    private DatabaseMovieStore(Connection connection) {
+        this.connection = connection;
+    }
+    public List<String> search(String yearParam){
+        if (yearParam != null) {
+            return queryByYear(Integer.parseInt(yearParam));
+        }
+        return queryAll();
+    }
+    private List<String> queryAll() {
+        return query("SELECT title FROM movies LIMIT 100");
+    }
+    private List<String> queryByYear(int year) {
+        return query("SELECT title FROM movies WHERE year = " + year);
+    }
+
+    private List<String> query(String sql) {
+        List<String> result = new ArrayList<>();
+        try(Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql)) {
+            while (rs.next()){
+                result.add(rs.getString("title"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
--- src/main/java/software/ulpgc/kata6/application/Main.java
+++ src/main/java/software/ulpgc/kata6/application/Main.java
@@ -1,6 +1,7 @@
 package software.ulpgc.kata6.application;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import software.ulpgc.kata6.architecture.io.Store;
 import software.ulpgc.kata6.architecture.model.Movie;
 
@@ -8,35 +9,33 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
-    private static final String database = "movies.db";
-    public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + database)) {
-            connection.setAutoCommit(false);
-            Desktop.with(moviesIn(connection))
-                    .display()
-                    .setVisible(true);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:movies.db");
+
+        DatabaseMovieStore store = DatabaseMovieStore.with(connection);
+
+        Javalin app = Javalin.create().start(7070);
+
+        app.get("/movies", ctx -> movies(ctx, store));
+    }
+        private static void movies(Context ctx, DatabaseMovieStore store){
+            String yearParam = ctx.queryParam("year");
+            try{
+                List<String> titles = store.search(yearParam);
+
+                if (titles.isEmpty()){
+                    ctx.status(400).result("No movies found");
+                    return;
+                }
+                ctx.result(String.join("\n", titles));
+            } catch (NumberFormatException e){
+                ctx.status(400).result("Invalid year");
+            }
         }
     }
-    private static Store moviesIn(Connection connection) throws SQLException {
-        if (isDatabaseEmpty()) importMoviesInto(connection);
-        return new DatabaseStore(connection);
-    }
 
-    private static void importMoviesInto(Connection connection) throws SQLException {
-        Stream<Movie> movies = new RemoteStore(MovieDeserializer::fromTsv).movies();
-        new DatabaseRecorder(connection).record(movies);
-        new DatabaseRecorder(connection);
-    }
-
-    private static boolean isDatabaseEmpty() {
-        return new File(database).length() == 0;
-    }
-
-
-
-}
